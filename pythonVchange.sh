c5b423e4aabe5d076ca86a375072011e6b20eb5a
mikebdict <66077988+mikebdict@users.noreply.github.com>
2020-08-10 14:57:50 +0100

    Add files via upload
    Small updates with some new things I've learned.

--- pythonVchange.sh
+++ pythonVchange.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Get the current version of python linked to /usr/bin/python.
-currentpyv=$(ls -l /usr/bin/python | awk '{print substr($11,10);}')
+currentpyv=$(readlink /usr/bin/python)
 echo 'The current python version is' $currentpyv
 echo 'Do you want to change it?'
-echo 'Enter y or n'
-read answer
-
-if test $answer = 'y';
-then
-# Create an array with the possible versions of python availible in /usr/bin.
-	posspyv=$( find /usr/bin -maxdepth 1 -type f -name 'python*' | awk '{print substr($1,10);}')
+read -s -n1 -p 'Enter Yes or No' answer
+case $answer in
+Y | y) echo
+echo 'Continueing'
+	# Create an array with the possible versions of python availible in /usr/bin.
+	posspyv=$( find /usr/bin -maxdepth 1 -type f -name 'python*' | awk '{print substr($1,10);}' )
 	posspyAR=($posspyv)
-# Use PS3 to pick a new version from that array to soflink, bail from the loop if a bad choice is made.
+	# Use PS3 to pick a new version from that array to soflink, bail from the loop if a bad choice is made.
 	PS3='Choose a version of python to soft link:'
 	select pyver in "${posspyAR[@]}"; do
 		if [[ $REPLY == "0" ]]; then
@@ -22,20 +21,21 @@ then
 		else
 			break
 		fi
-	done
-# Check that the program is actually doing something.
-		if test /usr/bin/$currentpyv = /usr/bin/$pyver ; then
+		done
+	# Check that the program is actually doing something.
+		test $currentpyv == /usr/bin/$pyver
+		if [[ $? == "0" ]]; then
 			echo 'That version of python is already softlinked, exiting'
 		else
-# Delete the currect python soflink, the create a new one and print it.
+		# Delete the currect python soflink, the create a new one and print it.
 			sudo rm /usr/bin/python
 			sudo ln -s /usr/bin/$pyver /usr/bin/python
 			newpyver=$( ls -l /usr/bin/python | awk '{print substr($11,10)}' )
 			echo 'the new softlinked python is' $newpyver 'cya'
-		fi
-else
+		fi;;
+N | n)
 # Mandatory double negative based snark. 
-echo 'Answer wasnt y, bye.'
-
-fi
+	echo 'Answer wasnt y, bye.'
+	exit;;
+esac
 exit 0
